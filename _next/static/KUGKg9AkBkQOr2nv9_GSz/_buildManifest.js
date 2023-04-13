9768c8fa71507583b8df77a3d9e3613a26d59760
Dredeco <Dredeco@users.noreply.github.com>
2023-04-13 19:09:11 +0000

    deploy: 62ce2e2516a6641af945fab59a6c53107092e74e

--- _next/static/KUGKg9AkBkQOr2nv9_GSz/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/942-24599666dc164037.js","static/css/256c44b4a923602f.css","static/chunks/pages/index-86305a833259c3fb.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
